1729236412dfe1f07d10249389f72175a2e52812
daimawanjiaA <2496347254@qq.com>
2022-06-13 14:50:57 +0800

    tre

--- src/main/java/service/_AdminService.java
+++
@@ -1,9 +0,0 @@
-package main.java.service;
-
-import main.java.entity.AdminDO;
-
-public interface _AdminService {
-    public boolean validateAdmin(AdminDO adminDo);//判定账号密码是否正确
-
-    public void startservice();//开始连接服务
-}
